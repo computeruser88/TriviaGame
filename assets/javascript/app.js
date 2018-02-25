1bc8735021002529939329ebe07d36e1d3c3b8d0
Jan Balangue <jantonio.balangue@gmail.com>
2018-02-24 17:09:33 -0800

    Modified index.html and style.css. Added trivia questions to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var questions = [
+    ["What is the large group of leg muscles that helps with knee extension?", "Quadratus lumborum", "Gastrocnemius", "Quadriceps", "Transverse abdominis", "C"],
+["What is the physiological mechanism through which fat loss happens?", "Perspiration", "Muscle contraction", "Digestion", "Respiration", "D"],
+["What is the largest muscle in the calf area?", "Gastrocnemius", "Soleus", "Iliotibial band", "Hamstring", "A"],
+["Which of the following is not a deltoid?", "Anterior", "Transverse", "Posterior", "Medial", "B"],
+["Which system is the group of glands that secretes hormones?", "Endocrine", "Cardiovascular", "Nervous", "Digestive", "A"],
+["Which muscle is not involved with elbow flexion?",  "Biceps brachii",  "Brachioradialis", "Biceps femoris","Brachialis", "C"],
+["Which system do the spinal erectors belong to?", "Skeletal", "Muscular", "Nervous", "Integumentary", "B"],
+["Which muscle is responsible for keeping the arm attached to the trunk of the body?", "Pectoralis Minor", "Biceps brachii", "Pectoralis Major", "Rectus abdominis", "C"],
+["Which is not a section of the spine?", "Cervical", "Lumbar", "Thoracic", "Sagittal", "D"],
+["Which of the following is considered a macronutrient?", "Potassium", "Carbohydrate", "Calcium", "Folate", "B"],
+["Which is not a monosaccharide?", "Glucose", "Galactose", "Lactose", "Fructose", "C"],
+["What is the immediate reserve of glucose for muscle cells called?", "Glycogen", "Glucagon", "Galactose", "Adenosine Triphosphate", "A"],
+["Which is considered the main anabolic hormone of the human body?", "Glucagon", "Insulin", "Estrogen", "Adrenaline", "B"],
+["Which is not a kind of muscle contraction?", "Isometric", "Concentric", "Eccentric", "Limbic", "D"],
+["What is not part of a carbohydrate molecule?", "Nitrogen", "Carbon", "Hydrogen", "Oxygen", "A"],
+["Where are femurs located?", "Upper arms", "Lower legs", "Torso", "Thighs", "D"]]
